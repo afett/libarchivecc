22b10ed3a1696580b38244217d94a2fb2f21820a
Andreas Fett <a.fett@gmx.de>
2019-05-20 21:54:14 +0200

    add Reader interface

---
+++ src/reader.cc
@@ -0,0 +1,18 @@
+/*
+   Copyright (c) 2019 Andreas Fett. All rights reserved.
+   Use of this source code is governed by a BSD-style
+   license that can be found in the LICENSE file.
+*/
+
+#include <archivecc/reader.h>
+
+namespace archivecc {
+
+class ReaderImpl : public Reader {
+public:
+private:
+};
+
+Reader::~Reader() = default;
+
+}
