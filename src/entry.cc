a7b84341380188ec1c504d9749abd9fa1cebeb8b
Andreas Fett <a.fett@gmx.de>
2019-05-25 22:03:13 +0200

    add Entry class

---
+++ src/entry.cc
@@ -0,0 +1,68 @@
+/*
+   Copyright (c) 2019 Andreas Fett. All rights reserved.
+   Use of this source code is governed by a BSD-style
+   license that can be found in the LICENSE file.
+*/
+
+#include <archivecc/entry.h>
+
+#include <archive_entry.h>
+#include <cassert>
+
+namespace archivecc {
+
+class EntryImpl : public Entry {
+public:
+	EntryImpl();
+
+private:
+	class Deleter {
+	public:
+		void operator()(archive_entry *ar) const noexcept
+		{
+			archive_entry_free(ar);
+		}
+	};
+
+	inline archive_entry *raw() const
+	{
+		return entry_.get();
+	}
+
+	std::unique_ptr<archive_entry, Deleter> entry_;
+};
+
+EntryImpl::EntryImpl()
+:
+	entry_(archive_entry_new())
+{
+	if (entry_ == nullptr) {
+		throw std::bad_alloc();
+	}
+}
+
+Entry::ptr Entry::create()
+{
+	return std::make_shared<EntryImpl>();
+}
+
+Entry::~Entry() = default;
+
+class EntryFactoryImpl : public EntryFactory {
+public:
+	Entry::ptr create_entry() const override;
+};
+
+Entry::ptr EntryFactory::create_entry() const
+{
+	return Entry::create();
+}
+
+EntryFactory::ptr EntryFactory::create()
+{
+	return std::make_shared<EntryFactoryImpl>();
+}
+
+EntryFactory::~EntryFactory() = default;
+
+}
